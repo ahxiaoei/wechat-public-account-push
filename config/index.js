f0f1ab1e4aca81648e379a5ebd46af3a45c148e4
ben <307208797@qq.com>
2022-09-13 20:52:29 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -41,7 +41,7 @@ export const config = {
       // 所在城市
       city: "深圳",
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
-      openUrl: "https://wangxinleo.cn"
+      openUrl: "http://ahxiao.com"
     }
   ],
 
