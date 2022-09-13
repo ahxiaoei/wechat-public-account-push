88824c8b35d29b9bdd4619813619058424aaba2a
ben <307208797@qq.com>
2022-09-13 20:55:26 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -35,7 +35,7 @@ export const config = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "oIVC_6l2NLuLxFJ258tl8ltywcU0",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "Hic96sxODQwPDGGQdAF4Qqojw0fit-xIkaDgot0wxf8",
+      useTemplateId: "rZiVYUDr7p5MPI2VxcO54ogeTX-hlhiHkMIDR1K-gDk",
       // 所在省份
       province: "广东",
       // 所在城市
