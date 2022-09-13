2aa2a458d6db62263ca79fa9897bc1bdd82d93ed
ben <307208797@qq.com>
2022-09-13 20:59:29 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -42,6 +42,20 @@ export const config = {
       city: "深圳",
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
       openUrl: "http://ahxiao.com"
+    },
+    {
+      // 想要发送的人的名字
+      name: "师妹",
+      // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+      id: "oIVC_6i65rDdoFFFmAvTsMacROt4",
+      // 你想对他发送的模板消息的模板ID
+      useTemplateId: "rZiVYUDr7p5MPI2VxcO54ogeTX-hlhiHkMIDR1K-gDk",
+      // 所在省份
+      province: "广东",
+      // 所在城市
+      city: "深圳",
+      // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
+      openUrl: "http://ahxiao.com"
     }
   ],
 
